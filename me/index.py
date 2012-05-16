3b4957b0e75d14dc0a0c3b890c3e7056df761cfb
Yuxin <bob.yuxinyang@gmail.com>
2012-05-16 18:40:03 +0800

    add basic info

--- me/index.py
+++ me/index.py
@@ -1,6 +1,7 @@
 #-*-coding=utf-8-*-
+from flask.templating import render_template
 from me import app
 
 @app.route('/', methods=['GET'])
 def g_index():
-    return 'Hello World'
+    return render_template('index.html')
